f16707de8d90adbd7669849d51ce11e37fe9afee
Breno Rocha <brenopenarocha2@gmail.com>
2022-03-24 16:58:41 -0300

    Script

---
+++ Super Trunfo/script.js
@@ -0,0 +1,530 @@
+let carta1 = {
+    nome: "Ezio Auditore",
+    imagem:
+      "http://pm1.narvii.com/6279/2f45fe73481891665b88c29d7a18a730bb5d6954_00.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 7,
+      magia: 1
+    }
+  };
+  
+let carta2 = {
+    nome: "Roronoa Zoro",
+    imagem: "https://c4.wallpaperflare.com/wallpaper/332/915/762/one-piece-roronoa-zoro-hd-wallpaper-preview.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 6,
+      magia: 7
+    }
+  };
+  
+let carta3 = {
+    nome: "Ikki de Fenix",
+    imagem:
+      "https://i.pinimg.com/originals/87/38/7f/87387f75d5af53e094124100a5bd19d0.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 7,
+      magia: 7
+    }
+  };
+  
+let carta4 = {
+    nome: "Exodia",
+    imagem:
+      "https://i.pinimg.com/474x/89/76/aa/8976aa0c5bc6fe103c1c1c81e4f424c6.jpg",
+    atributos: {
+      ataque: 999,
+      defesa: 999,
+      magia: 999
+    }
+  };
+  
+let carta5 = {
+    nome: "Batman",
+    imagem: "https://conteudo.imguol.com.br/c/entretenimento/04/2022/02/25/batman-1645790799911_v2_1x1.jpg",
+    atributos: {
+      ataque: 6,
+      defesa: 7,
+      magia: 2
+    }
+  };
+  
+let carta6 = {
+    nome: "Deadpool",
+    imagem:
+      "https://rollingstone.uol.com.br/media/uploads/deadpool.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 5,
+      magia: 2
+    }
+  };
+
+let carta7 = {
+    nome: "Iori Yagami",
+    imagem:
+      "https://a-static.besthdwallpaper.com/iori-yagami-chou-mobile-legends-ml-papel-de-parede-2560x1600-67171_7.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 5,
+      magia: 8
+    }
+  };
+  
+let carta8 = {
+    nome: "Kyo Kusanagi",
+    imagem: "http://pm1.narvii.com/6724/41e7fe25be4a0c26a052b6557c40b17909802fa1v2_00.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 6,
+      magia: 8
+    }
+  };
+  
+let carta9 = {
+    nome: "Monkey D. Luffy",
+    imagem:
+      "https://artesetra.com.br/wp-content/uploads/2021/01/20210102_one_piece_luffy.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 8,
+      magia: 6
+    }
+  };
+
+let carta10 = {
+    nome: "Geralt",
+    imagem:
+      "https://static.wixstatic.com/media/5f1962_62352a2a16f844588fa39c4ea460f1d1~mv2.jpg/v1/fill/w_800,h_432,al_c,q_90/5f1962_62352a2a16f844588fa39c4ea460f1d1~mv2.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 9,
+      magia: 10
+    }
+  };
+  
+let carta11 = {
+    nome: "Jill Valentine",
+    imagem: "https://spoiler.nerdaocubo.com.br/wp-content/uploads/2020/10/EeGwHUFU0AAb7nm-1-1-819x1024.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      magia: 4
+    }
+  };
+  
+let carta12 = {
+    nome: "Aloy",
+    imagem:
+      "https://ovicio.com.br/wp-content/uploads/2022/02/20220211-ovicio-aloy-ps5-horizon-forbidden-west.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 7,
+      magia: 6
+    }
+  };
+
+let carta13 = {
+    nome: "Johnny Joestar",
+    imagem:
+      "https://img-9gag-fun.9cache.com/photo/aK642gO_460s.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 4,
+      magia: 8
+    }
+  };
+  
+let carta14 = {
+    nome: "Dio Brando",
+    imagem: "http://pm1.narvii.com/7834/d0f49e2341e5d1d9d01036ca1c6068cb853ffa49r1-454-500v2_uhq.jpg",
+    atributos: {
+      ataque: 9,
+      defesa: 6,
+      magia: 9
+    }
+  };
+  
+let carta15 = {
+    nome: "Naruto",
+    imagem:
+      "https://cdn1.dotesports.com/wp-content/uploads/sites/3/2021/11/11111456/narutoooh-1536x864-1.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 6,
+      magia: 6
+    }
+  };
+
+let carta16 = {
+    nome: "Mikasa Ackerman",
+    imagem:
+      "https://sucodemanga.com.br/wp-content/uploads/2014/04/mikasa-shingeki-no-kyojin.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 6,
+      magia: 3
+    }
+  };
+  
+let carta17 = {
+    nome: "Goku",
+    imagem: "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/07/dragon-ball-super-1200x900-1.jpg",
+    atributos: {
+      ataque: 9,
+      defesa: 7,
+      magia: 8
+    }
+  };
+  
+let carta18 = {
+    nome: "Yusuke Urameshi",
+    imagem:
+      "http://pm1.narvii.com/6353/6a344996fccfaa5fca456c75df0895574ca7f88d_00.jpg",
+    atributos: {
+      ataque: 6,
+      defesa: 5,
+      magia: 9
+    }
+  };
+
+let carta19 = {
+    nome: "Megaman",
+    imagem:
+      "https://hipertextual.com/wp-content/uploads/2021/12/mega-man.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 5,
+      magia: 6
+    }
+  };
+  
+let carta20 = {
+    nome: "Sonic",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/as-melhores-fases-do-sonic-02_e1p4.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 6,
+      magia: 7
+    }
+  };
+  
+let carta21 = {
+    nome: "Homem de Ferro",
+    imagem:
+      "https://i.pinimg.com/originals/a7/f7/3a/a7f73ace67a9a9dd673e49ba913079f0.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 7,
+      magia: 8
+    }
+  };
+
+let carta22 = {
+    nome: "Dante",
+    imagem:
+      "https://www.pdvg.it/wp-content/uploads/2019/11/Devil-may-cry-3.jpg",
+    atributos: {
+      ataque: 9,
+      defesa: 7,
+      magia: 5
+    }
+  };
+  
+let carta23 = {
+    nome: "Ryu",
+    imagem: "http://pm1.narvii.com/6761/45de3d1264af41591278bcc032ee3764e11b33aav2_00.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      magia: 8
+    }
+  };
+  
+let carta24 = {
+    nome: "Wolverine",
+    imagem:
+      "https://i.pinimg.com/736x/a7/13/6c/a7136c053eb4128ccce2423060bec9bc.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 10,
+      magia: 3
+    }
+  };
+
+let carta25 = {
+    nome: "Super Choque",
+    imagem:
+      "https://p2.trrsf.com/image/fget/cf/1200/1200/filters:quality(85)/images.terra.com/2020/08/23/staticshockvol11textless.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 8,
+      magia: 9
+    }
+  };
+  
+let carta26 = {
+    nome: "Lanterna Verde",
+    imagem: "https://3.bp.blogspot.com/-Ub5ybOo5Oys/TX6o1S69lSI/AAAAAAAAGbE/lRzBCHBneco/s1600/kylejstwerart%25281%2529.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 6,
+      magia: 8
+    }
+  };
+  
+let carta27 = {
+    nome: "Samus",
+    imagem:
+      "https://observatoriodegames.uol.com.br/wp-content/uploads/2022/01/Metroid.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      magia: 4
+    }
+  };
+
+let carta28 = {
+    nome: "Blastoise",
+    imagem:
+      "https://www.tonica.la/__export/1611179849547/sites/debate/img/2021/01/20/carta-de-blastoise-se-vende-por-millones-de-dolares.jpg_1902800913.jpg",
+    atributos: {
+      ataque: 4,
+      defesa: 9,
+      magia: 7
+    }
+  };
+  
+let carta29 = {
+    nome: "Gutz",
+    imagem: "https://i.pinimg.com/originals/8a/e4/cd/8ae4cded4704dd7e82b396ee9dd8c5ec.jpg",
+    atributos: {
+      ataque: 9,
+      defesa: 6,
+      magia: 6
+    }
+  };
+  
+let carta30 = {
+    nome: "Pantera Negra",
+    imagem:
+      "https://besthqwallpapers.com/Uploads/3-1-2017/11830/thumb2-black-panther-4k-2018-movie-marvel-science-fiction.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      magia: 4
+    }
+  };
+
+let carta31 = {
+    nome: "Chun li",
+    imagem:
+      "https://cdn.awsli.com.br/600x450/1206/1206083/produto/126051202/0da0262e2e.jpg",
+    atributos: {
+      ataque: 6,
+      defesa: 6,
+      magia: 2
+    }
+  };
+  
+let carta32 = {
+    nome: "Kratos",
+    imagem: "https://cdn.awsli.com.br/1000x1000/1206/1206083/produto/91113374/f819ab8b25.jpg",
+    atributos: {
+      ataque: 10,
+      defesa: 5,
+      magia: 8
+    }
+  };
+  
+let carta33 = {
+    nome: "Harry Potter",
+    imagem:
+      "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/03/warner-prohibe-buhos-exhibiciones-vivo-harry-potter-2276705.jpg",
+    atributos: {
+      ataque: 4,
+      defesa: 5,
+      magia: 9
+    }
+  };
+  
+let carta34 = {
+    nome: "Chaves",
+    imagem:
+      "https://sm.ign.com/t/ign_br/screenshot/default/cropped-chaves-divulgacao3_83a3.h928@3:4.jpg",
+    atributos: {
+      ataque: 1,
+      defesa: 1,
+      magia: 1
+    }
+  };
+  
+let carta35 = {
+    nome: "Edward Kenway",
+    imagem: "https://static1.purebreak.com.br/articles/2/16/82/@/9845-assassin-s-creed-4-black-flag-e-o-950x0-2.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 7,
+      magia: 2
+    }
+  };
+  
+let carta36 = {
+    nome: "Vergil",
+    imagem:
+      "https://w0.peakpx.com/wallpaper/428/807/HD-wallpaper-vergil-devilmaycry-devilmaycry-3-dmc.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 8,
+      magia: 5
+    }
+  };
+
+let carta37 = {
+    nome: "Lara Croft",
+    imagem:
+      "https://laracroft.com.br/assets/uploads/2020/03/lara_croft_artwork-526x1024.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 6,
+      magia: 2
+    }
+  };
+  
+let carta38 = {
+    nome: "Ada Wong",
+    imagem: "https://img.gamewith.net/img/84ef1bc0b0fb18c033bf9e10e7c0230d.jpg",
+    atributos: {
+      ataque: 9,
+      defesa: 7,
+      magia: 4
+    }
+  };
+  
+let carta39 = {
+    nome: "Bayonetta",
+    imagem:
+      "https://www.comboinfinito.com.br/principal/wp-content/uploads/2018/02/1.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 5,
+      magia: 9
+    }
+  };
+
+let carta40 = {
+    nome: "Marco",
+    imagem:
+      "https://i.pinimg.com/originals/bd/07/a3/bd07a3dff9618bba8149486df4136cbb.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 5,
+      magia: 2
+    }
+  };
+
+let cartaMaquina;
+let cartaJogador;
+let cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10,
+              carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20,
+              carta21, carta22, carta23, carta24, carta25, carta26, carta27, carta28, carta29, carta30,
+              carta31, carta32, carta33, carta34, carta35, carta36, carta37, carta38, carta39, carta40];
+        
+
+
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 40);
+  cartaMaquina = cartas[numeroCartaMaquina];
+  
+  var numeroCartaJogador = parseInt(Math.random() * 40);
+  while (numeroCartaJogador == numeroCartaMaquina) {
+    numeroCartaJogador = parseInt(Math.random() * 40);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+  
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+  
+function jogar() {
+  console.log("chamou");
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+  
+  if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+    htmlResultado = "<p class='resultado-final'>Venceu</p>";
+  } 
+  else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+    htmlResultado = "<p class='resultado-final'>Perdeu</p>";
+  } 
+  else {
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+  
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+  
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+  
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
